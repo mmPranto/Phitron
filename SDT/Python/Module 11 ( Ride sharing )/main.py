b35a671a3884f3e74986badf94b130eade0959d1
Minhaz Ul Mostofa Pranto <zminhazul29@gmail.com>
2025-07-23 03:11:42 +0600

    Add files via upload

---
+++ SDT/Python/Module 11 ( Ride sharing )/main.py
@@ -0,0 +1,16 @@
+from ride import Ride,RideRequest,RideMatching,RideSharing
+from users import Rider,Driver
+from vehicle import Car,Bike
+
+niye_jao = RideSharing("Niye Jao")
+
+rahim = Rider("Rahim Uddin","rahim@gmail.com",1234,"Mohakhali",1200)
+niye_jao.add_rider(rahim)
+kolimuddin = Driver("Kolim Uddin","kolim@gmail.com",1256,"Gulshan")
+niye_jao.add_driver(kolimuddin)
+
+rahim.request_ride(niye_jao,"Uttara","car")
+# rahim.show_current_ride()
+kolimuddin.reach_destination(rahim.current_ride)
+# print(niye_jao)
+rahim.show_current_ride()
